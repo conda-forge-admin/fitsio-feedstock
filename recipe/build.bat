e6c3df0a1a21b4807da606ee74425b301190e348
Matthew R. Becker <beckermr@users.noreply.github.com>
2026-07-03 08:09:43 -0500

    Update build.bat for cfitsio installation process

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,6 +1,40 @@
+mkdir cfitsio-prefix
+
+tar -xzvf cfitsio-4.6.4.tzr.gz
+if errorlevel 1 exit 1
+
+cd cfitsio-4.6.4
+
+mkdir build
+cd build
+
+cmake -G "NMake Makefiles" ^
+  %CMAKE_ARGS% ^
+  -D CMAKE_PREFIX_PATH="..\..\cfitsio-prefix" ^
+  -D TESTS=On ^
+  -D UTILS=On ^
+  -D BUILD_SHARED_LIBS=Off ^
+  ..
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+:: test-ish programs (speed doesn't seem to get built)
+cookbook
+if errorlevel 1 exit 1
+testprog
+if errorlevel 1 exit 1
+  
+nmake install
+if errorlevel 1 exit 1
+
+cd ..
+cd ..
+
 set FITSIO_USE_SYSTEM_FITSIO="True"
-set FITSIO_SYSTEM_FITSIO_INCLUDEDIR="%LIBRARY_PREFIX%\include"
-set FITSIO_SYSTEM_FITSIO_LIBDIR="%LIBRARY_PREFIX%\lib"
+set FITSIO_SYSTEM_FITSIO_INCLUDEDIR="cfitsio-prefix"
+set FITSIO_SYSTEM_FITSIO_LIBDIR="cfitsio-prefix"
 
 %PYTHON% -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
 if errorlevel 1 exit 1
