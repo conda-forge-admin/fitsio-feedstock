52d0259a3bc21addb1e18fe97b69f6c7f15eeca4
Matthew R. Becker <beckermr@users.noreply.github.com>
2026-07-03 21:50:30 -0500

    Enable pthreads support in build script

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,4 +1,4 @@
-set CMAKE_ARGS=%CMAKE_ARGS% -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
+set CMAKE_ARGS=%CMAKE_ARGS% -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DUSE_PTHREADS=On
 
 %PYTHON% -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
 if errorlevel 1 exit 1
