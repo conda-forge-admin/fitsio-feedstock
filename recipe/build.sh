32b8ab6e59589a2d3c6c1a58fef87476804fb92d
Matthew R. Becker <beckermr@users.noreply.github.com>
2026-07-15 13:35:40 -0500

    Apply suggestion from @beckermr

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,5 @@
 # configure has a weird choice the ignores LDFLAGS
 export CFLAGS="${CFLAGS} ${LDFLAGS}"
 
-# Get an updated config.sub and config.guess
 
 $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
